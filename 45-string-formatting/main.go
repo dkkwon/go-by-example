9e12837ae31080d58f9c1458a8ca8fa175d18c1f
daeken.kwon <daeken.kwon@gmail.com>
2020-05-01 11:32:37 +0900

    string-formmating

--- 45-string-formatting/main.go
+++ 45-string-formatting/main.go
@@ -49,7 +49,7 @@ func main() {
 	// %s prints basic string formatting
 	fmt.Printf("%s\n", "hello")
 
-	// %p prints quoted string s
+	// %q prints quoted string s
 	fmt.Printf("%q\n", "hello")
 	// same as
 	fmt.Printf("%s\n", "\"hello\"")
@@ -70,4 +70,29 @@ func main() {
 	// works also for strings
 	fmt.Printf("|%6s|%6s|\n", "foo", "b")
 	fmt.Printf("|%-6s|%-6s|\n", "foo", "b")
+
+	// output:
+	// {1 2}
+	// {x:1 y:2}
+	// main.point{x:1, y:2}
+	// main.point
+	// true
+	// 123
+	// 1110
+	// !
+	// 1c8
+	// 78.900000
+	// 1.234000e+08
+	// 1.234000E+08
+	// "string"
+	// "\"string\""
+	// 6865782074686973
+	// 0x42135100
+	// |    12|   345|
+	// |  1.20|  3.45|
+	// |1.20  |3.45  |
+	// |   foo|     b|
+	// |foo   |b     |
+	// a string
+	// an error
 }
